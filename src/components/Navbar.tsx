45d1097b1fb5aa6ae120bd0152ad08a29e66cfca
Sourav nandi <118915260+sourav0009@users.noreply.github.com>
2024-09-01 14:44:01 +0530

    creat user and cart

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,7 @@
 import { FaSearch } from "react-icons/fa";
+import { FaUserCircle,FaShoppingCart } from "react-icons/fa";
+import CartCountBadge from "./CartCountBadge";
+
 
 const Navbar = () => {
   return (
@@ -8,8 +11,16 @@ const Navbar = () => {
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-black">Logo</span>
           <div className=" relative w-full max-w-[500px]">
             <input className=" bg-[#f2f3f5] border-none outline-none px-6 py-3 rounded-[30px] w-full" type="text" placeholder="Search Product..." />
-            <FaSearch className=" absolute top-0 right-0 mt-4 mr-5 text-gray-500" size={20}/>
-
+            <FaSearch className=" absolute top-0 right-0 mt-4 mr-5 text-gray-500" size={20} />
+          </div>
+          <div className=" flex gap-4">
+            <div className=" icon__wrapper">
+              <FaUserCircle />
+            </div>
+            <div className=" icon__wrapper relative">
+              <FaShoppingCart />
+              < CartCountBadge size="w-[25px] h-[25px]"/>
+            </div>
           </div>
         </div>
       </div>
