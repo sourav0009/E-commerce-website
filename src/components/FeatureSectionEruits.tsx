a1450a775c0da142531e9fa7d054327247826b40
Sourav nandi <118915260+sourav0009@users.noreply.github.com>
2024-09-04 17:23:21 +0530

    FeatureSectionEruits

--- src/components/FeatureSectionEruits.tsx
+++ src/components/FeatureSectionEruits.tsx
@@ -1,4 +1,11 @@
+import ProductCard from "./ProductCard"
 
+const data = [
+    { id:0, img: "product__1.webp", name: "Dried Mango", price: "$500"},
+    { id:1, img: "product__2.webp", name: "Crunchy Crispa", price: "$300"},
+    { id:2, img: "product__3.webp", name: "Jewel Cranberries", price: "$200"},
+    { id:3, img: "product__4.webp", name: "Almond organic", price: "$100"},
+];
 
 const FeatureSectionEruits = () => {
     return (
@@ -13,6 +20,12 @@ const FeatureSectionEruits = () => {
                     <button className=" text-gray-600 hover:text-accent">Vegetables</button>
                     <button className=" text-gray-600 hover:text-accent">Bread & Bakery</button>
                 </div>
+            </div>
+            <div className=" grid sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 pt-8 gap-2">
+                <div>
+                    <img className=" w-full h-full object-cover" src="/feature__1.webp" alt="banner" />
+                </div>
+                {data.map(el => <ProductCard key={el.id} img={el.img} name={el.name} price={el.price}/>)}
             </div>
         </div>
     )
---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,33 @@
+import { AiFillStar} from "react-icons/ai"
+import { CiStar } from "react-icons/ci";
+import { HiOutlineShoppingBag } from "react-icons/hi";
+interface propsType {
+    img:string;
+    name:string;
+    price:string;
+}
+
+
+const ProductCard:React.FC<propsType> = ({img,name,price}) => {
+  return (
+    <div className=" border border-gray-200 hover:border-gray-300 hover-scale-105  rounded-lg relative hs-accordion transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-150">
+      <img src={img} alt={name} />
+      <div className=" space-y-2 relative p-4">
+        <div className=" text-yellow-400 flex gap-[2px] text-[20px]">
+            <AiFillStar/>
+            <AiFillStar/>
+            <AiFillStar/>
+            <AiFillStar/>
+            <CiStar />
+        </div>
+            <h3 className=" font-medium">{name}</h3>
+            <h3 className=" text-2xl font-medium text-red-600">{price}</h3>
+            <div className=" absolute -top-4 right-2 bg-current text-white text-[28px] w-[50px] h-[50px] rounded-full grid place-items-center cursor-pointer">
+            <HiOutlineShoppingBag />
+            </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProductCard
