8449b4d75674793f6f665e64a42fa640771b007e
Sourav nandi <118915260+sourav0009@users.noreply.github.com>
2024-09-04 22:10:37 +0530

    BlogSection

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -1,1 +1,21 @@
+interface propsType{
+    img: string;
+    title: string
+    Comment: number;
+    date: string;
+}
 
+const BlogCard:React.FC<propsType> = ({img,title,Comment,date}) => {
+  return (
+    <div className=" space-y-4">
+        <img className=" rounded-lg hover:scale-105 transition-transform" src={img} alt="post" />
+        <div className=" text-accent font-medium">
+            <span>{date} / </span>
+            <span>{Comment} comments</span>
+        </div>
+        <h3 className=" font-bold text-xl">{title}</h3>
+    </div>
+  )
+}
+
+export default BlogCard;
--- src/components/BlogSection.tsx
+++ src/components/BlogSection.tsx
@@ -1,1 +1,47 @@
+import BlogCard from "./BlogCard";
 
+const data = [
+    {
+        img: "/post__1.webp",
+        title: "Healthy Food Healthy life",
+        date: "Age 27, 2023",
+        Comment: 8,
+    },
+    {
+        img: "/post__2.webp",
+        title: "Healthy Food Healthy life",
+        date: "Age 25, 2023",
+        Comment: 1,
+
+    },
+    {
+        img: "/post__3.webp",
+        title: "Healthy Food Healthy life",
+        date: "Age 30, 2023",
+        Comment: 6,
+
+    },
+];
+
+
+const BlogSection = () => {
+    return (
+        <div className=" container pt-16">
+            <h2 className="font-bold text-2xl">Latest News</h2>
+            <p className=" text-gray-500">Present Posts in a best way to highlight interesting moments of your blog</p>
+            <div className=" grid sm:grid-cols-2 md:grid-cols-3 gap-8 pt-8">
+                {data.map((el) => (
+                    <BlogCard
+                        key={el.date}
+                        img={el.img}
+                        title={el.title}
+                        date={el.date}
+                        Comment={el.Comment}
+                    />
+                ))}
+            </div>
+        </div >
+    )
+}
+
+export default BlogSection
